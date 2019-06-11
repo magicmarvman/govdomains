19bd528c81d0d4ca5060e91bd1eb19670485cd29
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-06-11 21:28:54 +0200

    Fix marker position

--- map.js
+++ map.js
@@ -16,7 +16,7 @@ function addMarkersByDomains() {
     geocoder =  new google.maps.Geocoder()
     domains.forEach((object) => {
         markers[object["Domain Name"]] = new google.maps.Marker({
-            position: myLatLng,
+            position: {lat: parseInt(object["Latitude"]), lng: parseInt(object["Longitude"])},
             map: map,
             title: object["Domain Name"]
         });
