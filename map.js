2250e7436e159e7f252028f3c82c71f7a04c457e
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-06-11 16:25:16 +0200

    Add javascript and map

---
+++ map.js
@@ -0,0 +1,28 @@
+let domains = null;
+let markers = [];
+
+function loadFromJson() {
+    fetch('domains.json')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonObj) {
+            domains = jsonObj
+        });
+}
+
+function addMarkersByDomains() {
+    geocoder =  new google.maps.Geocoder()
+    domains.forEach((object) => {
+        markers[object["Domain Name"]] = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: object["Domain Name"]
+        });
+    })
+}
+
+function start() {
+    loadFromJson()
+    addMarkersByDomains()
+}
