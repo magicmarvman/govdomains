68169a5b06a70c0591a668703cfee491fc792208
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-06-11 19:48:38 +0200

    Move file write block

--- latlngloader/index.mjs
+++ latlngloader/index.mjs
@@ -13,13 +13,11 @@ domains.forEach((part, index, object) => {
         domains[index]["Longitude"] = response[0]["lon"]
         domains[index]["OSM_Licence"] = response[0]["licence"]
         console.log(chalk.blue("Loading data ") + chalk.yellow(index) + chalk.blue(" of ") + chalk.yellow(domains.length) + chalk.blue("..."))
+        writeFileSync("./../domains_final.json", JSON.stringify(domains, null, 4))
     })
     .catch((error) => {
         console.log(error)
     })
 })
 
-console.log(chalk.yellow("Saving to file..."))
-writeFileSync("./../domains_final.json", JSON.stringify(domains, null, 4))
-console.log(chalk.green.bold("Done!"))
 
