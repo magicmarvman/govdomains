036a6d7c98f1857d333df94935e48cf1bf3e20fb
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-06-11 21:31:55 +0200

    Fix fetch request

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 let domains = null;
 let markers = [];
 
-function loadFromJson() {
+
+function addMarkersByDomains() {
     fetch('domains_final.json')
         .then(function(response) {
             return response.json();
@@ -9,21 +10,16 @@ function loadFromJson() {
         .then(function(jsonObj) {
             domains = jsonObj
             console.log(domains)
+            domains.forEach((object) => {
+                markers[object["Domain Name"]] = new google.maps.Marker({
+                    position: {lat: parseInt(object["Latitude"]), lng: parseInt(object["Longitude"])},
+                    map: map,
+                    title: object["Domain Name"]
+                });
+            })
         });
-}
-
-function addMarkersByDomains() {
-    geocoder =  new google.maps.Geocoder()
-    domains.forEach((object) => {
-        markers[object["Domain Name"]] = new google.maps.Marker({
-            position: {lat: parseInt(object["Latitude"]), lng: parseInt(object["Longitude"])},
-            map: map,
-            title: object["Domain Name"]
-        });
-    })
 }
 
 function start() {
-    loadFromJson()
     addMarkersByDomains()
 }
