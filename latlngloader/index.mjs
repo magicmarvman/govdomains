e4275a7ea9f1a58a798f4af2c13c88a7b9db5b18
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-06-11 19:46:14 +0200

    Add output

--- latlngloader/index.mjs
+++ latlngloader/index.mjs
@@ -12,12 +12,14 @@ domains.forEach((part, index, object) => {
         domains[index]["Latitude"] = response[0]["lat"]
         domains[index]["Longitude"] = response[0]["lon"]
         domains[index]["OSM_Licence"] = response[0]["licence"]
-        console.log()
+        console.log(chalk.blue("Loading data ") + chalk.yellow(index) + chalk.blue(" of ") + chalk.yellow(domains.length) + chalk.blue("..."))
     })
     .catch((error) => {
         console.log(error)
     })
 })
 
+console.log(chalk.yellow("Saving to file..."))
 writeFileSync("domains.json", JSON.stringify(domains, null, 4))
+console.log(chalk.green.bold("Done!"))
 
